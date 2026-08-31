121a9acdcf80f63887d7d12457eb21da460b24e2
rnglean <3474473003@qq.com>
2026-08-31 19:32:10 +0800

    Add MPDIoU bounding box loss

---
+++ models/loss_mpdiou.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+
+from ultralytics.utils.tal import bbox2dist
+from .mpdiou import bbox_mpdiou
+
+
+class DFLoss(nn.Module):
+    """Distribution Focal Loss."""
+
+    def __init__(self, reg_max=16):
+        super().__init__()
+        self.reg_max = reg_max
+
+    def __call__(self, pred_dist, target):
+        target = target.clamp_(0, self.reg_max - 1 - 0.01)
+        tl = target.long()
+        tr = tl + 1
+        wl = tr - target
+        wr = 1 - wl
+
+        return (
+            torch.nn.functional.cross_entropy(
+                pred_dist, tl.view(-1), reduction="none"
+            ).view(tl.shape) * wl
+            +
+            torch.nn.functional.cross_entropy(
+                pred_dist, tr.view(-1), reduction="none"
+            ).view(tl.shape) * wr
+        ).mean(-1, keepdim=True)
+
+
+class BboxLossMPDIoU(nn.Module):
+    """Bounding-box regression loss using MPDIoU."""
+
+    def __init__(self, reg_max=16):
+        super().__init__()
+        self.dfl_loss = DFLoss(reg_max) if reg_max > 1 else None
+
+    def forward(
+        self,
+        pred_dist,
+        pred_bboxes,
+        anchor_points,
+        target_bboxes,
+        target_scores,
+        target_scores_sum,
+        fg_mask,
+        mpdiou_hw,
+    ):
+        weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
+
+        iou = bbox_mpdiou(
+            pred_bboxes[fg_mask],
+            target_bboxes[fg_mask],
+            xywh=False,
+            mpdiou_hw=mpdiou_hw[fg_mask],
+        )
+
+        loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+
+        if self.dfl_loss:
+            target_ltrb = bbox2dist(
+                anchor_points,
+                target_bboxes,
+                self.dfl_loss.reg_max - 1,
+            )
+
+            loss_dfl = (
+                self.dfl_loss(
+                    pred_dist[fg_mask].view(-1, self.dfl_loss.reg_max),
+                    target_ltrb[fg_mask],
+                )
+                * weight
+            )
+
+            loss_dfl = loss_dfl.sum() / target_scores_sum
+        else:
+            loss_dfl = torch.tensor(0.0, device=pred_dist.device)
+
+        return loss_iou, loss_dfl
